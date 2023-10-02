61832b5026be73827c3bcdbe325b95f2cfbb7300
Arturo <carturolopezm@gmail.com>
2023-10-01 23:02:08 -0500

    Working send emails from UI

---
+++ client/src/components/Card.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import axios from 'axios'
+import { useState } from 'react'
+import { useFetch } from 'usehooks-ts'
+interface CardProps {
+  id: number
+  title: string
+  description: string
+  button: {
+    title: string
+    href: string
+  }
+  emails_sent: {
+    name: string
+    role: string
+  }
+}
+
+const Card: React.FC<{ card: CardProps }> = ({ card }) => {
+  const [data, setData] = useState([])
+  const actionEmail = async () => {
+    const response = await axios.post(card.button.href)
+    setData(response.data.responseItem)
+  }
+  return (
+    <article
+      key={card.id}
+      className='flex max-w-xl py-4 flex-col items-start justify-between px-2 shadow-md'>
+      <div className='group relative'>
+        <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
+          <div>
+            <span className='absolute inset-0' />
+            {card.title}
+          </div>
+        </h3>
+        <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
+          {card.description}
+        </p>
+      </div>
+      <div className='flex self-end items-center mt-2 gap-x-4 text-xs'>
+        <button
+          onClick={actionEmail}
+          className='relative z-10 rounded-full bg-black px-3 py-1.5 font-medium text-white '>
+          {card.button.title}
+        </button>
+      </div>
+      <div className='mt-2 flex items-center gap-x-4'>
+        <div className='text-sm leading-6'>
+          <p className='font-semibold text-gray-900'>{card.emails_sent.name}</p>
+          {/* <p className='text-gray-700'>{card.emails_sent.role}</p> */}
+        </div>
+      </div>
+      {data &&
+        data.map((email) => {
+          console.log('data', email)
+
+          return (
+            <div
+              className=' border w-full p-2'
+              key={email.licenseData!.id}>
+              <div>client: {email.clientData!.poc_email}</div>
+              <div>Admin: {email.adminData!.email_address}</div>
+            </div>
+          )
+        })}
+    </article>
+  )
+}
+
+export default Card
--- client/src/components/EmailSender.tsx
+++ client/src/components/EmailSender.tsx
@@ -1,11 +1,16 @@
-const posts = [
+import Card from './Card'
+
+const cards = [
   {
     id: 1,
     title: 'Send Licenses emails 4 months',
     href: '#',
     description:
       'Send email notification to the users who are point of contact of clients which licenses will expire in four months.',
-    category: { title: 'Send now', href: '#' },
+    button: {
+      title: 'Send now',
+      href: 'http://localhost:8080/api/licenses/send-email/four-months'
+    },
     emails_sent: {
       name: 'Emails sent',
       role: 'Co-Founder / CTO',
@@ -18,9 +23,11 @@ const posts = [
     href: '#',
     description:
       'Send email notification to the users who are point of contact of clients which licenses will expire in 1 month only if today is Monday.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Send now', href: '#' },
+
+    button: {
+      title: 'Send now',
+      href: 'http://localhost:8080/api/licenses/send-email/one-month'
+    },
     emails_sent: {
       name: 'Emails sent',
       role: 'Co-Founder / CTO',
@@ -33,9 +40,11 @@ const posts = [
     href: '#',
     description:
       'Send email notification to the users who are point of contact of clients which licenses will expire in 1 week.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Send now', href: '#' },
+
+    button: {
+      title: 'Send now',
+      href: 'http://localhost:8080/api/licenses/send-email/one-week'
+    },
     emails_sent: {
       name: 'Emails sent',
       role: 'Co-Founder / CTO',
@@ -48,9 +57,11 @@ const posts = [
     href: '#',
     description:
       'Send email notification to the users who are point of contact of clients which licenses will expire in any of the previous scenarios.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Send now', href: '#' },
+
+    button: {
+      title: 'Send now',
+      href: 'http://localhost:8080/api/licenses/send-email/all'
+    },
     emails_sent: {
       name: 'Emails sent',
       role: 'Co-Founder / CTO',
@@ -66,49 +77,16 @@ const EmailSender = () => {
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto max-w-2xl lg:mx-0'>
           <h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-            From the blog
+            Notifier
           </h2>
           <p className='mt-2 text-lg leading-8 text-gray-600'>
-            Learn how to grow your business with our expert advice.
+            Notify your staff that one or more of its clients have licenses that
+            could expire soon.
           </p>
         </div>
         <div className='mx-auto mt-4 grid max-w-2xl grid-cols-1 gap-x-4  border-t border-gray-200    lg:mx-0 lg:max-w-none lg:grid-cols-4'>
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className='flex max-w-xl py-4 flex-col items-start justify-between px-2 shadow-md'>
-              <div className='group relative'>
-                <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
-                  <div>
-                    <span className='absolute inset-0' />
-                    {post.title}
-                  </div>
-                </h3>
-                <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
-                  {post.description}
-                </p>
-              </div>
-              <div className='flex self-end items-center  gap-x-4 text-xs'>
-                {/* <time
-                  dateTime={post.datetime}
-                  className='text-gray-500'>
-                  {post.date}
-                </time> */}
-                <button
-                  //   href={post.category.href}
-                  className='relative z-10 rounded-full bg-black px-3 py-1.5 font-medium text-white '>
-                  {post.category.title}
-                </button>
-              </div>
-              <div className=' mt-2 flex items-center gap-x-4'>
-                <div className='text-sm leading-6'>
-                  <p className='font-semibold text-gray-900'>
-                    {post.emails_sent.name}
-                  </p>
-                  <p className='text-gray-700'>{post.emails_sent.role}</p>
-                </div>
-              </div>
-            </article>
+          {cards.map((card) => (
+            <Card card={card} />
           ))}
         </div>
       </div>
