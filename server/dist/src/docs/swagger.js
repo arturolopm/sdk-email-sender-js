f90022877f4acac1feea42f5f50526c92f06decc
Arturo <carturolopezm@gmail.com>
2023-10-04 08:43:08 -0500

    added schemas for documentation

--- server/dist/src/docs/swagger.js
+++ server/dist/src/docs/swagger.js
@@ -17,7 +17,7 @@ const swaggerDefinition = {
     ],
     components: {
         schemas: {
-            user: {
+            User: {
                 type: 'object',
                 required: ['first_name', 'last_name', 'email_address', 'isStaff'],
                 properties: {
@@ -32,6 +32,70 @@ const swaggerDefinition = {
                     },
                     isStaff: {
                         type: 'boolean'
+                    }
+                }
+            },
+            Client: {
+                type: 'object',
+                required: ['poc_email', 'poc_name', 'admin_poc'],
+                properties: {
+                    poc_name: {
+                        type: 'string'
+                    },
+                    poc_email: {
+                        type: 'string',
+                        unique: true
+                    },
+                    admin_poc: {
+                        type: 'integer',
+                        description: 'The ID of the user who is the primary admin for this client.',
+                        references: {
+                            model: 'User',
+                            key: 'id'
+                        }
+                    }
+                }
+            },
+            License: {
+                type: 'object',
+                required: ['package', 'license_type', 'client_id'],
+                properties: {
+                    package: {
+                        type: 'string',
+                        enum: ['javascript_sdk', 'ios_sdk', 'android_sdk']
+                    },
+                    license_type: {
+                        type: 'string',
+                        enum: ['production', 'evaluation']
+                    },
+                    client_id: {
+                        type: 'integer',
+                        references: {
+                            model: 'Client',
+                            key: 'id'
+                        }
+                    },
+                    created_datetime: {
+                        type: 'date'
+                    },
+                    expiration_datetime: {
+                        type: 'date'
+                    }
+                }
+            },
+            Email: {
+                type: 'object',
+                required: ['sent_at', 'license_id'],
+                properties: {
+                    sent_at: {
+                        type: 'date'
+                    },
+                    license_id: {
+                        type: 'integer',
+                        references: {
+                            model: 'License',
+                            key: 'id'
+                        }
                     }
                 }
             }
